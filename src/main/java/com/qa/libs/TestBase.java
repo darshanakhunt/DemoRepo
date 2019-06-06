093fb30a0755d107c722fb67455d130bf9f1e374
Your Name <you@example.com>
2019-06-06 19:02:36 +0530

    First git commit for my selenium code

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class TestBase {
+	
+	public void print()
+	{
+		System.out.println("Test Method");
+	}
+
+}
---
+++ src/main/java/com/qa/pages/loginPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class loginPage {
+
+	public void loginpage()
+	{
+		System.out.println("Login Page");
+	}
+}
